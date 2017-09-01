1fd334777834392cf7f5fbef5594349b8e4fd9b8
Pooky <gps-i@foxmail.com>
2017-09-01 22:51:06 +0800

    添加闪屏页面

--- app/src/main/java/activity/SplashActivity.java
+++ app/src/main/java/activity/SplashActivity.java
@@ -3,8 +3,10 @@ package activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.Button;
 
 import com.pooky.demo.R;
 
@@ -14,20 +16,45 @@ import com.pooky.demo.R;
  */
 
 public class SplashActivity extends Activity {
-    private final int SPLASH_DISPLAY_LENGHT = 2000;
+    private final int SPLASH_DISPLAY_LENGHT = 2200;
+    private Button btn_splash_close;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
+        btn_splash_close = (Button) findViewById(R.id.btn_splash_close);
+/*        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
-                SplashActivity.this.startActivity(intent);
-                SplashActivity.this.finish();
+                gotoMainActivity();
             }
-        },SPLASH_DISPLAY_LENGHT);
+        }, SPLASH_DISPLAY_LENGHT);*/
+        countDownTimer.start();
     }
 
+    private CountDownTimer countDownTimer = new CountDownTimer(SPLASH_DISPLAY_LENGHT,1000) {
+        @Override
+        public void onTick(long l) {
+            btn_splash_close.setText("跳过 " + l / 1000);
+        }
 
+        @Override
+        public void onFinish() {
+            btn_splash_close.setText("跳过 " + 0 );
+            gotoMainActivity();
+        }
+    };
+
+    private void gotoMainActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        SplashActivity.this.startActivity(intent);
+        SplashActivity.this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        countDownTimer.cancel();
+    }
 }
